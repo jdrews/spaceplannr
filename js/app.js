dc46d2ea2f464b0e2cb5f5b101c2061b6150a707
Jon Drews <jon@jondrews.com>
2018-09-05 23:31:40 -0400

    restructuring for editable fields

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+// ====== setup map interface =======
+
 // create the map
 var map = L.map('map', {
     center: [0, 0],
@@ -22,13 +24,20 @@ map.addControl(drawControl);
 // add draw layers upon creation
 map.on(L.Draw.Event.CREATED, function (event) {
     var layer = event.layer;
-    layer.bindTooltip("John Doe");
+    //layer.id = //todo: generate a UUID to assign to this layer. Set the layer-id hidden field to this value for after the user clicks save. look at node-uuid library.
+    // layer.bindTooltip("John Doe");
     layer.on({
         click: function (e) {
+            // todo: update the profile sidebar with user clicked data
             sidebar.open("profile");
             L.DomEvent.stop(e); // kill event
         }
     });
+    //todo: make profile sidebar editable and add the save button
+    console.log("layer.id = " + layer.id);
+    $("#layer-id").val(layer.id);
+    console.log("layer-id = " + $("layer-id").val());
+    sidebar.open("profile");
     drawnItems.addLayer(layer);
 });
 // if we didn't click anything else, close the sidebar
@@ -37,6 +46,16 @@ map.on('click', function () {
 });
 
 var sidebar = L.control.sidebar('sidebar', {position: 'right'}).addTo(map);
+
+// ====== setup form =======
+$("#profile-save").click(function () {
+    var name = $("profile-name").val();
+    var chat = $("profile-chat").val();
+    var email = $("profile-email").val();
+
+});
+
+// ====== begin pouchdb =======
 
 var syncDom = document.getElementById('sync-wrapper');
 var db = new PouchDB('spaceplannr');
